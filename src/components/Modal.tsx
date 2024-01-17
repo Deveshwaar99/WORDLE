adad0e5181448856cd3f829111f08dbd6231432f
Deveshwaar99 <deveshwaar99@gmail.com>
2024-01-17 12:55:15 +0530

    chore(modal): update modal description

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -16,28 +16,42 @@ type ModalProps = {
 }
 
 export default function Modal({ word, showModal, isSuccess, setShowModal }: ModalProps) {
-  return (
+  const wordDescription = (
     <>
-      <Dialog open={showModal} onOpenChange={() => setShowModal(false)}>
-        <DialogTrigger>Open</DialogTrigger>
-        <DialogContent>
-          <DialogHeader>
-            <DialogTitle>
-              {isSuccess ? <span>Congratulations!</span> : <span>Unlucky!</span>}
-            </DialogTitle>
-            <DialogDescription>
-              {isSuccess ? (
-                <span>You have successfully guessed the word</span>
-              ) : (
-                <span>
-                  The correct word is{' '}
-                  <span className=" font-bold size-10">{word.toUpperCase()}</span>
-                </span>
-              )}
-            </DialogDescription>
-          </DialogHeader>
-        </DialogContent>
-      </Dialog>
+      <span className="font-bold text-xl">{word.toUpperCase()}</span>
+      <div className="text-gray-600 mt-2">Come back tomorrow for a new word challenge! 👋</div>
     </>
+  )
+
+  return (
+    <Dialog open={showModal} onOpenChange={() => setShowModal(false)}>
+      <DialogTrigger>Open</DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>
+            {isSuccess ? (
+              <span className="text-green-500">Congratulations!</span>
+            ) : (
+              <span className="text-red-500">Unlucky!</span>
+            )}
+          </DialogTitle>
+          <DialogDescription>
+            {isSuccess ? (
+              <>
+                <span className="text-gray-700">You have successfully guessed the word </span>
+                {wordDescription}
+              </>
+            ) : (
+              <>
+                <span className="text-gray-700">
+                  Unfortunately, you did not guess the correct word this time.
+                </span>
+                {wordDescription}
+              </>
+            )}
+          </DialogDescription>
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
   )
 }
