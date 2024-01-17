faf73fe0b80a88de0dfa14b66654834a32215d1d
Deveshwaar99 <deveshwaar99@gmail.com>
2024-01-17 13:29:25 +0530

    feat(utils): add saveDataToLocalStorage function

---
+++ src/utils/saveDataToLocalStorage.ts
@@ -0,0 +1,10 @@
+function saveDataToLocalStorage(isSuccess: boolean, word: string) {
+  const today = new Date()
+  const date = today.toLocaleDateString()
+  const data = {
+    date,
+    isSuccess,
+  }
+  const jsonData = JSON.stringify(data)
+  localStorage.setItem('wordStatus', jsonData)
+}
