dd7ab5a05cfcfb8cd425a293f2b9988ccc73eab1
Deveshwaar <deveshwaar99@gmail.com>
2024-05-19 01:39:11 +0530

    fix : get  storage  values before playing

--- src/app/GamePage.tsx
+++ src/app/GamePage.tsx
@@ -1,26 +1,44 @@
 'use client'
+
 import WordlePage from '@/app/WordlePage'
 import Modal from '@/components/Modal'
 import { getDataFromLocalStorage } from '@/utils/getDataFromLocalStorage'
-import { useEffect, useState } from 'react'
+import { useEffect, useMemo, useRef, useState } from 'react'
 type GamePageProps = {
   word: string
 }
+let count = 0
 export default function GamePage({ word }: GamePageProps) {
+  const currentStreakRef = useRef(0)
+  const successStateRef = useRef(false)
   const [showModal, setShowModal] = useState(false)
-  const [isSuccess, setIsSuccess] = useState(false)
-  useEffect(() => {
-    const data = getDataFromLocalStorage('wordStatus')
-    const today = new Date().toLocaleDateString()
-    if (data && data.date === today) {
-      setShowModal(true)
-      if (data.isSuccess) setIsSuccess(data.isSuccess)
-    }
-  }, [])
+  const [loading, setLoading] = useState(true)
 
-  return showModal ? (
-    <Modal word={word} isSuccess={isSuccess} showModal setShowModal={() => {}} />
-  ) : (
-    <WordlePage word={word} />
-  )
+  useEffect(() => {
+    async function getLocalStorageValues() {
+      const storageValues = await getDataFromLocalStorage()
+      if (!storageValues) return
+      const today = new Date().toLocaleDateString()
+
+      if (storageValues.date === today && storageValues.word === word) {
+        currentStreakRef.current = storageValues.streak
+        successStateRef.current = storageValues.isSuccess
+        setShowModal(true)
+      }
+    }
+    getLocalStorageValues()
+  }, [word])
+
+  if (showModal) {
+    return (
+      <Modal
+        word={word}
+        isSuccess={successStateRef.current}
+        showModal
+        currentStreak={currentStreakRef.current}
+      />
+    )
+  }
+
+  return <WordlePage word={word} currentStreak={currentStreakRef.current} />
 }
