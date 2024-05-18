c8763a45748c04e7d150c08a475b4857be18e19b
Deveshwaar <deveshwaar99@gmail.com>
2024-05-19 01:19:05 +0530

    feat : add function to get data fro local storage

--- src/utils/getDataFromLocalStorage.ts
+++ src/utils/getDataFromLocalStorage.ts
@@ -1,6 +1,23 @@
-export function getDataFromLocalStorage(key: string) {
-  const data = localStorage.getItem(key)
-  if (!data) return null
-  const { date, isSuccess } = JSON.parse(data)
-  return { date, isSuccess }
+import { decryptSymmetric } from './encryption'
+
+export async function getDataFromLocalStorage() {
+  const encryptedPayload = localStorage.getItem('encryptedPayload')
+  const initializationVector = localStorage.getItem('initializationVector')
+  const stringTagValue = localStorage.getItem('stringTagValue')
+
+  if (!encryptedPayload || !initializationVector || !stringTagValue) return null
+  const storageObject = await decryptSymmetric(
+    encryptedPayload,
+    initializationVector,
+    stringTagValue
+  )
+
+  const { word, date, isSuccess, streak } = JSON.parse(storageObject)
+
+  return { word, date, isSuccess, streak } as {
+    word: string
+    date: string
+    isSuccess: boolean
+    streak: number
+  }
 }
