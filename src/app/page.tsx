47fe8cb064c6bbe5e8e13aa88a56901f5dfc2d8a
Deveshwaar <deveshwaar99@gmail.com>
2024-05-19 13:26:40 +0530

    ui: Adjust  page size

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,18 @@
 //components
 import GamePage from '@/app/GamePage'
 //utils
+import AppHeader from '@/components/AppHeader/AppHeader'
 import { fetchWord } from './fetchword'
 //theme
 
 export default async function Home() {
   const { word } = await fetchWord()
   return (
-    <>
-      <GamePage word={word} />
-    </>
+    <div className="m-auto">
+      <div className="flex flex-col items-center justify-center gap-y-5">
+        <AppHeader />
+        <GamePage word={word} />
+      </div>
+    </div>
   )
 }
